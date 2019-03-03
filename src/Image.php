5de602850c4adaed83f59d5a6f93727337ff3547
Rick Strafy <rixafy@gmail.com>
2019-03-03 23:24:12 +0100

    Add Image entity

---
+++ src/Image.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rixafy\Blog;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Selectable;
+use Rixafy\Doctrination\EntityTranslator;
+use Rixafy\DoctrineTraits\ActiveTrait;
+use Rixafy\DoctrineTraits\DateTimeTrait;
+use Rixafy\DoctrineTraits\UniqueTrait;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Table(name="image")
+ */
+class Image extends EntityTranslator
+{
+    use UniqueTrait;
+    use ActiveTrait;
+    use DateTimeTrait;
+
+    /**
+     * @Translatable
+     * @var string
+     */
+    protected $url_name;
+
+    /**
+     * @Translatable
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @Translatable
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @Translatable
+     * @var string
+     */
+    protected $alternative_text;
+
+    /**
+     * @return string
+     */
+    public function getUrlName(): string
+    {
+        return $this->url_name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlternativeText(): string
+    {
+        return $this->alternative_text;
+    }
+
+    /**
+     * @return Selectable
+     */
+    public function getTranslations()
+    {
+        // TODO: Implement getTranslations() method.
+    }
+}
