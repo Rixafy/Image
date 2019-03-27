b913f85b6c8b13e6415c7aeea4e7a72987b610a4
Rick Strafy <rixafy@gmail.com>
2019-03-27 09:04:58 +0100

    Remove ImageGroup::$images

--- src/Group/ImageGroup.php
+++ src/Group/ImageGroup.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace Rixafy\Image\ImageGroup;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
 use Rixafy\DoctrineTraits\DateTimeTrait;
 use Rixafy\DoctrineTraits\UniqueTrait;
-use Rixafy\Image\Image;
 
 /**
  * @ORM\Entity
@@ -26,17 +24,8 @@ class ImageGroup
      */
     private $name;
 
-    /**
-     * One ImageGroup has Many Images
-     *
-     * @ORM\OneToMany(targetEntity="\Rixafy\Image\Image", mappedBy="image_group", cascade={"persist", "remove"})
-     * @var Image[]
-     */
-    private $images;
-
     public function __construct(ImageGroupData $imageGroupData)
     {
-        $this->images = new ArrayCollection();
         $this->edit($imageGroupData);
     }
 
@@ -51,13 +40,5 @@ class ImageGroup
     public function getName(): string
     {
         return $this->name;
-    }
-
-    /**
-     * @return Image[]
-     */
-    public function getImages()
-    {
-        return $this->images;
     }
 }
